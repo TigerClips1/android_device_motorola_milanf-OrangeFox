c334a2a0a13f209296907fcbfe0c4d36fbff605a
TigerClips1 <111101615+TigerClips1@users.noreply.github.com>
2026-09-05 23:44:08 -0400

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -40,6 +40,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export FOX_VERSION="R12.1"
 	export FOX_VARIANT="A13"
 	export FOX_BUILD_TYPE="Stable"
+	
 	# OrangeFox Addons
 	export FOX_ENABLE_APP_MANAGER=1
 	
