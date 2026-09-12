1ed489bf451fa27b8024fc06f945dcb7e03a7c94
TigerClips1 <111101615+TigerClips1@users.noreply.github.com>
2026-09-11 22:47:09 -0400

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -46,7 +46,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	
 	# Binaries & Tools
 	export FOX_USE_BASH_SHELL=1
-	export FOX_ASH_IS_BASH=1
+	export FOX_ASH_IS_BASH=0
 	export FOX_USE_NANO_EDITOR=1
 	export FOX_USE_TAR_BINARY=1
 	export FOX_USE_SED_BINARY=1
